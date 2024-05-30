83484b84a3774a3f62cce91127b8e0df6e9157b8
iamhabi <iamhabi@pm.me>
2024-05-30 12:26:45 +0900

    set index 0 if get groups

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
 import group.GroupViewBigLayout
 import group.GroupViewSmallLayout
@@ -57,6 +58,8 @@ fun App() {
     if (taskGroups.size == 0) {
         TaskClient.getGroups { group ->
             taskGroups.add(group)
+
+            selectedGroupIndex.value = 0
         }
     }
 
