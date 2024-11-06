3af9da3ed71592e9188068aa1cae966faa62c51c
iamhabi <github.unzip957@slmail.me>
2024-11-06 10:45:55 +0900

    go HomeScreen if login valid

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,17 +4,29 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './HomeScreen';
 import TaskDetailScreen from './TaskDetailScreen';
+import LoginScreen from './LoginScreen';
+import RegisterScreen from './RegisterScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function RootLayout() {
+  const initialRouteName = getInitialRouteName();
+
   return (
     <SafeAreaProvider>
       <NavigationContainer independent={true}>
         <Stack.Navigator
-          initialRouteName='Home'
+          initialRouteName={initialRouteName}
           screenOptions={{ headerShown: false }}
         >
+          <Stack.Screen
+            name='Login'
+            component={LoginScreen}
+          />
+          <Stack.Screen
+            name='Register'
+            component={RegisterScreen}
+          />
           <Stack.Screen
             name='Home'
             component={HomeScreen}
@@ -27,4 +39,12 @@ export default function RootLayout() {
       </NavigationContainer>
     </SafeAreaProvider>
   );
+}
+
+function getInitialRouteName() {
+  return isLoginValid() ? 'Home' : 'Login';
+}
+
+function isLoginValid() {
+  return true;
 }
