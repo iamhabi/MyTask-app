ac04fbbd3448a979ae2ab6581107a346812dd919
iamhabi <github.unzip957@slmail.me>
2024-11-06 09:35:41 +0900

    LoginScreen

---
+++ app/LoginScreen.tsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { Keyboard, StyleSheet, Text, TextInput, TouchableHighlight, TouchableWithoutFeedback, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function LoginScreen() {
+  const [id, setID] = useState('');
+  const [password, setPassword] = useState('');
+
+  const requestLogin = () => {
+    // TODO Request login
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          style={styles.inputFieldContainer}
+          onChangeText={setID}
+          value={id}
+          placeholder="ID"
+          autoFocus
+        />
+
+        <TextInput
+          style={styles.inputFieldContainer}
+          onChangeText={setPassword}
+          value={password}
+          placeholder="Password"
+        />
+
+        <View style={{
+            margin: 16,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}
+        >
+          <TouchableHighlight
+            style={[
+              styles.buttonContainer,
+              {
+                borderWidth: 0.5,
+                backgroundColor: '#EEEEEE',
+              }
+            ]}
+            underlayColor='#CECECE'
+            onPress={() => {
+
+            }}
+          >
+            <Text>Register</Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight
+            style={[
+              styles.buttonContainer,
+              { backgroundColor: '#0088FF', }
+            ]}
+            underlayColor='#1166DD'
+            onPress={() => {
+              requestLogin();
+            }}
+          >
+            <Text style={{ color: 'white' }}>Login</Text>
+          </TouchableHighlight>
+        </View>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 8,
+  },
+  inputFieldContainer: {
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 8,
+    margin: 16,
+  },
+  buttonContainer: {
+    borderRadius: 8,
+    padding: 8,
+  },
+});
