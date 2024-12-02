9cdf9c74ed79de543a9a804646492d555e750eca
iamhabi <github.unzip957@slmail.me>
2024-12-02 09:45:29 +0900

    get/add/delete task

--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -5,12 +5,29 @@ import TaskInputField from '@/components/TaskInputField';
 import TaskItem from '@/components/TaskItem';
 import { useAppNavigation } from '@/types/navigation';
 import { ROUTES } from '@/constants/routes';
-import { useTaskContext } from '@/hooks/TaskContext';
+import { useServerContext } from '@/hooks/ServerContext';
+import { useEffect, useState } from 'react';
 
 export default function HomeScreen() {
   const navigation = useAppNavigation()
   
-  const { tasks, addTask, deleteTask } = useTaskContext()
+  const [isUp, setUp] = useState(false)
+  const { tasks, getTasks, addTask, deleteTask } = useServerContext()
+
+  useEffect(() => {
+    if (!isUp) {
+      getTasks(
+        () => {
+          
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+
+      setUp(true)
+    }
+  })
 
   return (
     <SafeAreaView style={styles.container}>
@@ -28,14 +45,30 @@ export default function HomeScreen() {
               })
             }}
             onDelete={(deletedTask) => {
-              deleteTask(deletedTask.id)
+              deleteTask(
+                deletedTask.id,
+                () => {
+                  // Task deleted
+                },
+                (error) => {
+
+                }
+              )
             }}
           />
         }
       />
       <TaskInputField
         onCreate={(title, description, dueDate) => {
-          addTask(undefined, title, description, dueDate)
+          addTask(
+            undefined, title, description, dueDate,
+            () => {
+              // Task added
+            },
+            (error) => {
+              console.error(error)
+            }
+          )
         }}
       />
     </SafeAreaView>
